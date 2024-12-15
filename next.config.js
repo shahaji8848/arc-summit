871011dd228e8661e26d39198e76d341ac7fe433
shahaji8848 <155730078+shahaji8848@users.noreply.github.com>
2024-12-15 12:59:00 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
   // Example of handling environment variables
   env: {
     // Ensure your backend API or service URL is set correctly here
-    API_URL: process.env.API_URL || 'http://127.0.0.1:36529', // Default fallback
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:36529', // Client-side
+    API_URL: process.env.API_URL || 'http://127.0.0.1', // Default fallback
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1', // Client-side
   },
 
   // Additional configuration options can be added here if needed
